300d9da113632f77f110098f892e69f0e3b7f606
Double Yoo <doubleyoo@Doubles-MacBook-Pro.local>
2021-07-17 16:12:54 -0400

    initial commit

---
+++ DB /connection.js
@@ -0,0 +1,22 @@
+const mysql = require('mysql')
+const util = require('util')
+
+// create the connection information for the sql database
+const connection = mysql.createConnection({
+    host: 'localhost',
+  
+    // Your port; if not 3306
+    port: 3306,
+  
+    // Your username
+    user: 'root',
+  
+    // Your password
+    password: '',
+    database: 'greatBay_DB',
+  });
+connection.connect()
+
+connection.query = util.promisify(connection.query)
+
+module.exports = connection
